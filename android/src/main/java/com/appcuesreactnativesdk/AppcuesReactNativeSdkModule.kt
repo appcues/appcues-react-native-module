e744d728528d72ef2fba7000829490fe6ddafd0f
James Ellis <james@appcues.com>
2022-02-03 15:16:07 -0500

    ✨ Add initial support for Android SDK wrapper

--- android/src/main/java/com/appcuesreactnativesdk/AppcuesReactNativeSdkModule.kt
+++ android/src/main/java/com/appcuesreactnativesdk/AppcuesReactNativeSdkModule.kt
@@ -1,24 +1,56 @@
 package com.appcuesreactnativesdk
 
+import com.appcues.Appcues
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap
 
 class AppcuesReactNativeSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    private lateinit var implementation: Appcues
 
     override fun getName(): String {
         return "AppcuesReactNativeSdk"
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+    fun setup(accountID: String, applicationID: String) {
+        val context = reactApplicationContextIfActiveOrWarn
+        val activity = currentActivity
+        if (context != null && activity != null) {
+          implementation = Appcues.Builder(context, accountID, applicationID)
+            .activity(activity)
+            .logging(Appcues.LoggingLevel.BASIC).build()
+        }
     }
 
-    
+    @ReactMethod
+    fun identify(userID: String, properties: ReadableMap? = null) {
+        implementation.identify(userID, properties?.toHashMap())
+    }
+
+    @ReactMethod
+    fun screen(title: String, properties: ReadableMap? = null) {
+        implementation.screen(title, properties?.toHashMap())
+    }
+
+    @ReactMethod
+    fun track(name: String, properties: ReadableMap? = null) {
+        implementation.track(name, properties?.toHashMap())
+    }
+
+    @ReactMethod
+    fun show(experienceID: String) {
+        currentActivity?.runOnUiThread {
+            implementation.show(experienceID)
+        }
+    }
+
+    @ReactMethod
+    fun debug() {
+        currentActivity?.runOnUiThread {
+            implementation.debug()
+        }
+    }
 }
