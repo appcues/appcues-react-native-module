740be6f1b73cf4a195d8a687dc5d36cfc00cfea7
James Ellis <james@appcues.com>
2022-02-22 22:30:11 -0500

    ♻️ Update init with useEffect

--- example/App.js
+++ example/App.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as Appcues from 'appcues-react-native-sdk'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -18,41 +18,50 @@ export default function App() {
   // in the view hierarchy (sign in screen, profile screen)
   const [userID, setUserID] = useState('default-00000');
 
-  const firstUpdate = useRef(true);
-  useLayoutEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      Appcues.setup('ACCOUNT_ID', 'APP_ID')
+  // Ensures that first _real_ render of the app doesn't occur until
+  // SDK init complete - to avoid screen view analytics before SDK is ready
+  const [initComplete, setInitComplete] = useState(false)
+
+  useEffect(() => {
+    if (initComplete) {
       return;
     }
+    Appcues.setup('ACCOUNT_ID', 'APP_ID')
+    setInitComplete(true)
   });
 
   return (
     <UserContext.Provider value={{ userID, setUserID }}>
-      <NavigationContainer theme={{
-        ...DefaultTheme,
-        colors: {
-          ...DefaultTheme.colors,
-          primary: '#1EB5C4',
-          background: `#FFFFFF`,
-          border: `#FFFFFF`
-        },
-      }}>
-        <RootStack.Navigator >
-          <RootStack.Screen name="Sign In" component={SignInScreen}
-            options={({ navigation }) => ({
-              headerShadowVisible: false,
-              headerTitleStyle: { fontWeight: '600' },
-              headerRight: () => (
-                <PlainButton title="Skip"
-                  onPress={() => {
-                    navigation.navigate('Main')
-                  }} />)
-            })} />
-          <RootStack.Screen name="Main" component={MainScreen}
-            options={{ presentation: 'fullScreenModal', animation: 'fade', headerShown: false }} />
-        </RootStack.Navigator>
-      </NavigationContainer>
+      {initComplete && <RootView/>}
     </UserContext.Provider>
+  );
+}
+
+function RootView() {
+  return (
+    <NavigationContainer theme={{
+      ...DefaultTheme,
+      colors: {
+        ...DefaultTheme.colors,
+        primary: '#1EB5C4',
+        background: `#FFFFFF`,
+        border: `#FFFFFF`
+      },
+    }}>
+      <RootStack.Navigator >
+        <RootStack.Screen name="Sign In" component={SignInScreen}
+          options={({ navigation }) => ({
+            headerShadowVisible: false,
+            headerTitleStyle: { fontWeight: '600' },
+            headerRight: () => (
+              <PlainButton title="Skip"
+                onPress={() => {
+                  navigation.navigate('Main')
+                }} />)
+          })} />
+        <RootStack.Screen name="Main" component={MainScreen}
+          options={{ presentation: 'fullScreenModal', animation: 'fade', headerShown: false }} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
