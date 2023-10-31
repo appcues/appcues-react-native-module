44d852bfd1190fde2902bdd18a4c67b32e26b858
James Ellis <james@appcues.com>
2023-10-31 15:44:55 -0400

    test with a modal that can have content overlaid

--- example/screens/main/EventsScreen.js
+++ example/screens/main/EventsScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Modal, StyleSheet, Text, Pressable, View } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as Appcues from '@appcues/react-native';
@@ -14,8 +14,33 @@ const EventsView = () => {
     }, [])
   );
 
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onShow={() => Appcues.track('modal shown')}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              nativeID="btnHideModal"
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
       <TintedButton
         title="Trigger Event 1"
         nativeID="btnEvent1"
@@ -25,6 +50,13 @@ const EventsView = () => {
         title="Trigger Event 2"
         nativeID="btnEvent2"
         onPress={() => Appcues.track('event2')}
+      />
+      <TintedButton
+        title="Show Modal"
+        nativeID="btnShowModal"
+        onPress={() => {
+          setModalVisible(true);
+        }}
       />
     </View>
   );
@@ -55,5 +87,46 @@ const styles = StyleSheet.create({
     paddingTop: 35,
     paddingLeft: 40,
     paddingRight: 40,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
