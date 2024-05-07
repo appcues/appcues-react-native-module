05224458100909bc465920fe5006b78573a08145
Matt Hayashida <matt.hayashida@appcues.com>
2024-05-07 13:48:07 -0400

    🎬 Add iOS Notification Service Extension

---
+++ example/ios/NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,11 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by Matt on 2024-05-07.
+//
+
+import UserNotifications
+import AppcuesNotificationService
+
+class NotificationService: AppcuesNotificationServiceExtension {}
