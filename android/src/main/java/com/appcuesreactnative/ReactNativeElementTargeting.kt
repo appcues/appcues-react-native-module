9f2b8e8028f89c76c63f122645557af40d6bc910
Matt Hayashida <matt.hayashida@appcues.com>
2025-07-28 09:42:55 -0400

    ✨ Capture target elements from web views

--- android/src/main/java/com/appcuesreactnative/ReactNativeElementTargeting.kt
+++ android/src/main/java/com/appcuesreactnative/ReactNativeElementTargeting.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.view.inspector.WindowInspector
 import androidx.core.view.children
 import com.appcues.ElementSelector
@@ -14,36 +15,55 @@ import com.appcues.ElementTargetingStrategy
 import com.appcues.ViewElement
 import com.appcues.isAppcuesView
 import com.facebook.react.R
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import org.json.JSONArray
 import java.lang.reflect.Method
 
-internal data class ReactNativeViewSelector(var nativeId: String?, var testId: String?): ElementSelector {
+internal data class ReactNativeViewSelector(
+  var appcuesId: String?, var nativeId: String?, var testId: String?, var tag: String?
+): ElementSelector {
 
     val isValid: Boolean
-        get() = nativeId != null || testId != null
+        get() = appcuesId != null || nativeId != null || testId != null || tag != null
 
     val displayName: String?
         get() = when {
+            appcuesId != null -> appcuesId
             nativeId != null -> nativeId
             testId != null -> testId
+            tag != null -> "(tag $tag)"
             else -> null
         }
 
     override fun toMap(): Map<String, String> =
         mapOf(
+            "appcuesID" to appcuesId,
             "nativeID" to nativeId,
-            "testID" to testId
+            "testID" to testId,
+            "tag" to tag
         ).filterValues { it != null }.mapValues { it.value as String }
 
     override fun evaluateMatch(target: ElementSelector): Int {
         var weight = 0
 
         (target as? ReactNativeViewSelector)?.let {
+            if (!it.appcuesId.isNullOrEmpty() && it.appcuesId == appcuesId) {
+                weight += 1_000
+            }
+
             if (!it.nativeId.isNullOrEmpty() && it.nativeId == nativeId) {
                 weight += 1_000
             }
 
             if (!it.testId.isNullOrEmpty() && it.testId == testId) {
                 weight += 1_000
+            }
+
+            if (it.tag != null && it.tag == tag) {
+              weight += 100
             }
         }
 
@@ -91,7 +111,7 @@ internal class ReactNativeViewTargeting(
     private val module: AppcuesReactNativeModule
 ): ElementTargetingStrategy {
 
-    override fun captureLayout(): ViewElement? {
+    override suspend fun captureLayout(): ViewElement? {
         return module.activity?.getParentView()?.let {
             val screenBounds = Rect()
             it.getGlobalVisibleRect(screenBounds)
@@ -101,13 +121,15 @@ internal class ReactNativeViewTargeting(
 
     override fun inflateSelectorFrom(properties: Map<String, String>): ElementSelector? {
         return ReactNativeViewSelector(
+            appcuesId = properties["appcuesID"],
             nativeId = properties["nativeID"],
-            testId = properties["testID"]
+            testId = properties["testID"],
+            tag = properties["tag"]
         ).let { if (it.isValid) it else null }
     }
 }
 
-private fun View.asCaptureView(screenBounds: Rect): ViewElement? {
+private suspend fun View.asCaptureView(screenBounds: Rect): ViewElement? {
     val displayMetrics = context.resources.displayMetrics
     val density = displayMetrics.density
 
@@ -128,41 +150,134 @@ private fun View.asCaptureView(screenBounds: Rect): ViewElement? {
         return null
     }
 
-    var children = (this as? ViewGroup)?.children?.mapNotNull {
-        if (!it.isShown) {
-            // discard hidden views and subviews within
-            null
-        } else {
-            it.asCaptureView(screenBounds)
-        }
-    }?.toList()
+    val children: MutableList<ViewElement> = mutableListOf()
+    val rectDp = globalVisibleRect.toDp(density)
 
-    if (children?.isEmpty() == true) {
-        children = null
+    if (this is ViewGroup) {
+        coroutineScope {
+            val deferred = this@asCaptureView.children.map {
+                async {
+                    if (!it.isShown) {
+                        // discard hidden views and subviews within
+                        null
+                    } else {
+                        it.asCaptureView(screenBounds)
+                    }
+                }
+            }.toList()
+
+            val viewChildren = deferred.awaitAll().filterNotNull()
+            children.addAll(viewChildren)
+        }
     }
 
-    val selector = selector()
+  if (this is WebView) {
+     children.addAll(this.children(rectDp))
+  }
+
+  val selector = selector()
 
     return ViewElement(
-        x = globalVisibleRect.left.toDp(density),
-        y = globalVisibleRect.top.toDp(density),
-        width = globalVisibleRect.width().toDp(density),
-        height = globalVisibleRect.height().toDp(density),
+        x = rectDp.left,
+        y = rectDp.top,
+        width = rectDp.width(),
+        height = rectDp.height(),
         selector = selector,
         displayName = selector?.displayName,
         type = this.javaClass.name,
-        children = children,
+        children = children.ifEmpty { null },
     )
 }
 
 internal fun View.selector(): ReactNativeViewSelector? {
     return ReactNativeViewSelector(
+        appcuesId = null,
         // captures "nativeID" property set on a react native View
         nativeId = getTag(R.id.view_tag_native_id)?.toString()?.ifEmpty { null },
         // captures "testID" property set on a react native View
         testId = getTag(R.id.react_test_id)?.toString()?.ifEmpty { null },
+        tag = null
     ).let { if (it.isValid) it else null }
+}
+
+
+private suspend fun WebView.children(rectDp: Rect): List<ViewElement> {
+  val js = """
+        [...document.querySelectorAll('[id], [data-appcues-id]')].reduce((result, el) => {
+            const { x, y, width, height } = el.getBoundingClientRect();
+            const tag = el.id ? `#${'$'}{el.id}` : null;
+            const appcuesID = el.getAttribute('data-appcues-id')
+            if (height !== 0 && width !== 0) {
+                result.push({
+                    x,
+                    y,
+                    width,
+                    height,
+                    tag,
+                    appcuesID
+                });
+            }
+            return result;
+        }, []);
+        """
+
+  val result = evaluateJavascript(js)
+  return result.mapNotNull { el ->
+    val x = (el["x"] as Double).toInt()
+    val y = (el["y"] as Double).toInt()
+    val width = (el["width"] as Double).toInt()
+    val height = (el["height"] as Double).toInt()
+
+    val appcuesId = el["appcuesID"] as? String
+    val tag = el["tag"] as? String
+
+    // considering it eligible for targeting if the center point is visible
+    val centerX = (x + (width / 2.0)).toInt()
+    val centerY = (y + (height / 2.0)).toInt()
+
+    if (centerX in 0..rectDp.width() && centerY in 0..rectDp.height())
+      ViewElement(
+        x = rectDp.left + x,
+        y = rectDp.top + y,
+        width = width,
+        height = height,
+        displayName = appcuesId ?: tag,
+        selector = ReactNativeViewSelector(
+          appcuesId = appcuesId,
+          nativeId = null,
+          testId = null,
+          tag = el["tag"] as String?
+        ),
+        type = "HTMLNode",
+        children = null,
+      )
+    else null
+  }
+}
+
+private suspend fun WebView.evaluateJavascript(script: String): List<Map<String, Any>> {
+    val completion = CompletableDeferred<List<Map<String, Any>>>()
+    evaluateJavascript(script) { result ->
+        try {
+            val jsonArray = JSONArray(result)
+            val parsedList = (0 until jsonArray.length()).map { i ->
+                val jsonObject = jsonArray.getJSONObject(i)
+                jsonObject.keys().asSequence().associateWith { jsonObject[it] }
+            }
+            completion.complete(parsedList)
+        } catch (e: Exception) {
+            completion.completeExceptionally(e)
+        }
+    }
+    return completion.await()
 }
 
 private fun Int.toDp(density: Float) =
     (this / density).toInt()
+
+private fun Rect.toDp(density: Float) = Rect(
+    (this.left / density).toInt(),
+    (this.top / density).toInt(),
+    (this.right / density).toInt(),
+    (this.bottom / density).toInt()
+)
