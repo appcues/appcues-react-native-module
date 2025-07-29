6d00dd344189913fc4061128c854c3fc914df2a2
Matt Hayashida <matt.hayashida@appcues.com>
2025-07-29 12:21:00 -0400

    🔧 Fix typo in build

--- example/android/fastlane/replace-placeholders.sh
+++ example/android/fastlane/replace-placeholders.sh
@@ -1,12 +1,12 @@
 # replace placeholders
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/APPCUES_ACCOUNT_ID/$1/g" ../../src/App.tsc
-  sed -i '' -e "s/APPCUES_APPLICATION_ID/$2/g" ../../src/App.tsc
+  sed -i '' -e "s/APPCUES_ACCOUNT_ID/$1/g" ../../src/App.tsx
+  sed -i '' -e "s/APPCUES_APPLICATION_ID/$2/g" ../../src/App.tsx
   sed -i '' -e "s/APPCUES_APPLICATION_ID/$2/g" ../app/src/main/AndroidManifest.xml
   sed -i '' -e "s/GOOGLE_SERVICES_KEY/$3/g" ../app/google-services.json
 else
-  sed -i -e "s/APPCUES_ACCOUNT_ID/$1/g" ../../src/App.tsc
-  sed -i -e "s/APPCUES_APPLICATION_ID/$2/g" ../../src/App.tsc
+  sed -i -e "s/APPCUES_ACCOUNT_ID/$1/g" ../../src/App.tsx
+  sed -i -e "s/APPCUES_APPLICATION_ID/$2/g" ../../src/App.tsx
   sed -i -e "s/APPCUES_APPLICATION_ID/$2/g" ../app/src/main/AndroidManifest.xml
   sed -i -e "s/GOOGLE_SERVICES_KEY/$3/g" ../app/google-services.json
 fi
